eaace83bd82fbd33fc7a1bad80ec84fefbf0475f
Mark Luo <mluo9987@gmail.com>
2024-08-14 16:56:54 -0400

    Completed JS exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degrees) {
+  return Math.round(((degrees - 32) * 5/9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return Math.round((celsius * 9/5 + 32) * 10) / 10;
 };
+
 
 // Do not edit below this line
 module.exports = {
